a0daee489bb74330bc40b5717d46764ac9a35509
YU-Ruiyang123 <147825908+YU-Ruiyang123@users.noreply.github.com>
2023-12-04 17:05:54 +0800

    Search System
    Co-Authored-By: CHENLetian123 <147827601+CHENLetian123@users.noreply.github.com>
    Co-Authored-By: XUHaoyang949 <147829214+XUHaoyang949@users.noreply.github.com>
    Co-Authored-By: WangYifan1024 <147827414+WangYifan1024@users.noreply.github.com>

--- Books Management System/src/com/clt/bookmanager/BookManager.java
+++ Books Management System/src/com/clt/bookmanager/BookManager.java
@@ -31,4 +31,33 @@ public class BookManager {
     public void deleteBook(String isbn) {
         books.removeIf(book -> book.getIsbn().equals(isbn));
     }
+    public List<Book> searchByTitle(String title) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public List<Book> searchByAuthor(String author) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().toLowerCase().contains(author.toLowerCase())) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public Book searchByIsbn(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;}
 }
+
+
