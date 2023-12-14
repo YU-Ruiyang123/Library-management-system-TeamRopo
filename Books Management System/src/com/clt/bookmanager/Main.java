0cd707b205d2ff71abcf46258f245bce480b9af0
XUHaoyang949 <2406724227@qq.com>
2023-12-14 22:29:38 +0800

    Fixed a bug
    Fixed a bug where the program crashed when the input was "Final".
    
    Co-Authored-By: WangYifan1024 <147827414+WangYifan1024@users.noreply.github.com>

--- Books Management System/src/com/clt/bookmanager/Main.java
+++ Books Management System/src/com/clt/bookmanager/Main.java
@@ -22,6 +22,7 @@ package com.clt.bookmanager;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.InputMismatchException;
 
 public class Main {
     private static BookManager manager = new BookManager(); // 用于管理书籍的对象
@@ -30,45 +31,52 @@ public class Main {
     public static void main(String[] args) {
         manager.loadBooksFromFile(); // 程序启动时加载书籍数据
         while (true) {
-            //System.out.println("Current working directory: " + System.getProperty("user.dir")); // 获取程序运行时的工作目录
-            System.out.println("\n图书管理系统:");
-            System.out.println("1. 添加书籍");
-            System.out.println("2. 显示所有书籍");
-            System.out.println("3. 更新书籍");
-            System.out.println("4. 删除书籍");
-            System.out.println("5. 搜索书籍");
-            System.out.println("6. 退出");
-            System.out.print("请输入您的选择: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // 清除输入行的换行符
+            displayMenu();
+            try {
+                System.out.print("请输入您的选择: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // 清除输入行的换行符
 
-            switch (choice) {
-                case 1:
-                    addBook();
-                    manager.saveBooksToFile(); // 添加书籍后,保存数据到文件data/books.csv
-                    break;
-                case 2:
-                    displayAllBooks();
-                    break;
-                case 3:
-                    updateBook();
-                    manager.saveBooksToFile(); // 更新书籍后,保存数据到文件data/books.csv
-                    break;
-                case 4:
-                    deleteBook();
-                    manager.saveBooksToFile(); // 删除书籍后,d保存数据到文件data/books.csv
-                    break;
-                case 5:
-                    searchBooks(); // 调用搜索书籍的方法
-                    break;
-                case 6:
-                    System.out.println("退出系统。");
-                    // 注意：不再关闭scanner，避免关闭System.in
-                    return;
-                default:
-                    System.out.println("无效的选择，请重新输入！");
+                switch (choice) {
+                    case 1:
+                        addBook();
+                        manager.saveBooksToFile(); // 添加书籍后,保存数据到文件data/books.csv
+                        break;
+                    case 2:
+                        displayAllBooks();
+                        break;
+                    case 3:
+                        updateBook();
+                        manager.saveBooksToFile(); // 更新书籍后,保存数据到文件data/books.csv
+                        break;
+                    case 4:
+                        deleteBook();
+                        manager.saveBooksToFile(); // 删除书籍后,d保存数据到文件data/books.csv
+                        break;
+                    case 5:
+                        searchBooks(); // 调用搜索书籍的方法
+                        break;
+                    case 6:
+                        System.out.println("退出系统。");
+                        return;
+                    default:
+                        System.out.println("无效的选择，请重新输入！");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("输入错误，请输入有效的数字选项！");
+                scanner.nextLine(); // 清除错误输入
             }
         }
+    }
+
+    private static void displayMenu() {
+        System.out.println("\n图书管理系统:");
+        System.out.println("1. 添加书籍");
+        System.out.println("2. 显示所有书籍");
+        System.out.println("3. 更新书籍");
+        System.out.println("4. 删除书籍");
+        System.out.println("5. 搜索书籍");
+        System.out.println("6. 退出");
     }
 
     private static void addBook() {
